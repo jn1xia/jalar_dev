e06fb495c7f7355f6ae5d2688be913034457851a
jevoned <48shield@gmail.com>
2021-08-31 20:50:36 +0700

    Tutorial

---
+++ BackEnd/Tutorial/path_parameters.py
@@ -0,0 +1,8 @@
+#Path Parameters: Get an Item by ID
+#You can declare path parameters or variables with the same syntax used by Python formatted strings:
+
+from fastapi import FastAPI
+
+app = FastAPI()
+
+@app.get
